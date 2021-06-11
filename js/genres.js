7ee6e647051d5b28466731f8a5fc5353f64523b1
quillenbucciero <abuccierorodriguez@udesa.edu.ar>
2021-06-11 11:36:53 -0300

    generos js, ya no me marca error pero no funciona

--- js/genres.js
+++ js/genres.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", function(){ //Evento que controla que todo el html esté cargado en el navegador (window se carga antes que document)
 
 
-/*
+
 
 
     let url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre";
@@ -15,28 +15,30 @@ window.addEventListener("load", function(){ //Evento que controla que todo el ht
             //aca muestro código
             console.log(data);
             let info = data.data;
-    
             //capturamos los destinos
             let generos = document.querySelector('.conenedor_generos');
+            let contenidoGeneros = ''; //poner el contenido a mostrar dentro de la sección
 
             for(let i=0; i<info.length; i++){
-                nombreGenero.innerHTML = <article class="genres">   
+                contenidoGeneros += ` <article class="genres">   
 
                                             <a href="detail-genres.html?id=${info[i].id}"><img src="${info.picture_medium}" alt="genre imagen"></a>
                                             <h4><a href="detail-genres.html">${info.name}</a></h4>
 
-                                        </article>
-                                    
-
+                                        </article> ` //las comillas invertidas son muy importates!
             }
+
+            generos.innerHTML += contenidoGeneros
+
+            
             
         })
         .catch( function(error){
             console.log(error);
         })
 
-        //genero.length
-*/
+        
+
 
 
 
