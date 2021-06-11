e77465609d8fe4c82a2d801c18eeefacdb7d9164
quillenbucciero <abuccierorodriguez@udesa.edu.ar>
2021-06-11 10:56:07 -0300

    avance en js generos y gif loader terminado

---
+++ js/genres.js
@@ -0,0 +1,68 @@
+
+window.addEventListener("load", function(){ //Evento que controla que todo el html esté cargado en el navegador (window se carga antes que document)
+
+
+/*
+
+
+    let url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre";
+        
+    fetch ( url ) //consultando la API
+        .then (function (response){
+            return response.json();
+        })
+        .then (function (data){
+            //aca muestro código
+            console.log(data);
+            let info = data.data;
+    
+            //capturamos los destinos
+            let generos = document.querySelector('.conenedor_generos');
+
+            for(let i=0; i<info.length; i++){
+                nombreGenero.innerHTML = <article class="genres">   
+
+                                            <a href="detail-genres.html?id=${info[i].id}"><img src="${info.picture_medium}" alt="genre imagen"></a>
+                                            <h4><a href="detail-genres.html">${info.name}</a></h4>
+
+                                        </article>
+                                    
+
+            }
+            
+        })
+        .catch( function(error){
+            console.log(error);
+        })
+
+        //genero.length
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}) 
+
